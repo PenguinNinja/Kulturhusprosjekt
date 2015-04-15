a8ff774c2259f13d46ad65589944f028e7b2c6ef
VeggTeppe <kikko.pett@gmail.com>
2015-04-15 12:08:11 +0200

    Revert "Revert "asdf""
    This reverts commit 0e465d59fb407c5743a46a28395e0288fdc5de3d.
    
    Revert "asdf"
    
    This reverts commit
    75d685b118986bdeccd719fb123b94620af0aa4c.
    
    asdf
    
    Skrev en viktig kodebit

--- Kulturhusprogram/src/kulturhusprogram/Kulturhusprogram.java
+++ Kulturhusprogram/src/kulturhusprogram/Kulturhusprogram.java
@@ -15,7 +15,12 @@ public class Kulturhusprogram {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
+        //HAAAAALLLOOOOOOO!=!=!=!!
+                
+         
     }
     
 }
