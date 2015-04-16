c427b26e533e6ac11b5c3a25db6c55cc02a56fc5
PenguinNinja <magneaaopdal@Hotmail.com>
2015-04-16 13:20:00 +0200

    Update Kulturhusprogram.java

--- Kulturhusprogram/src/kulturhusprogram/Kulturhusprogram.java
+++ Kulturhusprogram/src/kulturhusprogram/Kulturhusprogram.java
@@ -1,27 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kulturhusprogram;
 
-/**
- *
- * @author Magne!!
- */
+
 public class Kulturhusprogram {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
-        
-        
-        helkasdfalkasdjklfsdjklsdjklfsadjkl
-                ækjsdaljkfsdkljdfsa
-                
-                
+        Adminwindow adminvindu = new Adminwindow();
     }
     
 }
