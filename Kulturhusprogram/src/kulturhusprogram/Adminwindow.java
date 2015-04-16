1929965c8a2163af3409b1b9b1085feb97584e5b
PenguinNinja <magneaaopdal@Hotmail.com>
2015-04-16 13:07:39 +0200

    Adminvindu
    asdasd

---
+++ Kulturhusprogram/src/kulturhusprogram/Adminwindow.java
@@ -0,0 +1,93 @@
+package kulturhusprogram;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class Adminwindow extends JFrame{
+    private JTextField searchField;
+    private JTextArea place, contact, event;
+    private JButton adminPlace, adminContact, adminEvent;
+    
+    public Adminwindow(){
+        super("Administrering");
+        
+        Container pane = getContentPane();
+        
+        pane.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        
+        c.weighty = 1;
+        c.gridx = 1;
+        c.gridy = 0;
+        
+        pane.add(new Label("Administrering av kulturhuset"), c);
+        
+        c.weightx = 1;
+        c.gridx = 2;
+        c.anchor = GridBagConstraints.FIRST_LINE_END;
+        
+        searchField = new JTextField(10);
+        searchField.setText("søk");
+        
+        pane.add(searchField, c);
+        
+        c.weighty = 0;
+        c.weightx = 0.5;
+        c.anchor = GridBagConstraints.CENTER;
+        c.gridx = 0;
+        c.gridy = 1;
+       // c.fill = GridBagConstraints.BOTH;
+        c.ipady = 50;
+        
+        place = new JTextArea(20,25);
+        place.setEditable(false);
+        pane.add(place, c);
+                
+        c.gridx = 1;
+        
+        contact = new JTextArea(20,25);
+        contact.setEditable(false);
+        
+        pane.add(contact, c);
+        
+        c.gridx = 2;
+        
+        event = new JTextArea(20,25);
+        event.setEditable(false);
+        
+        pane.add(event, c);
+        
+        adminPlace = new JButton("Administrer lokaler");
+        
+        c.gridx = 0;
+        c.gridy = 2;
+        c.ipady = 0;
+        c.weighty = 1;
+        
+        pane.add(adminPlace, c);
+        
+        c.gridx = 1;
+        
+        adminContact = new JButton("Administrer kontaktperson");
+        
+        pane.add(adminContact, c);
+        
+        c.gridx = 2;
+        
+        adminEvent = new JButton("Administrer arrangement");
+        
+        pane.add(adminEvent, c);
+        
+        setSize(1000,500);
+        setVisible(true);
+    }
+    
+    public void setVisible(){
+        setVisible(true);
+    }
+    public void setInvisible(){
+        setVisible(false);
+    }
+}
--- Kulturhusprogram/src/kulturhusprogram/Kulturhusprogram.java
+++ Kulturhusprogram/src/kulturhusprogram/Kulturhusprogram.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kulturhusprogram;
 
-/**
- *
- * @author Magne
- */
 public class Kulturhusprogram {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Adminwindow adminvindu = new Adminwindow();
     }
     
 }
